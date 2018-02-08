6ae59f69ec7c2cf9e725c3a0eace7129b220c878
Kalab Oster <kalaboster@gmail.com>
2018-02-07 21:27:05 -0800

    Write change to vars and commit for state.

--- eval.py
+++ eval.py
@@ -17,12 +17,12 @@ import csv
 #tf.flags.DEFINE_string("positive_data_file", "./data/rt-polaritydata/rt-polarity.pos", "Data source for the positive data.")
 #tf.flags.DEFINE_string("negative_data_file", "./data/rt-polaritydata/rt-polarity.neg", "Data source for the negative data.")
 tf.flags.DEFINE_string("positive_data_file", "./data/The_Detective_Store/test_narrative.txt", "Narrative.")
-tf.flags.DEFINE_string("negative_data_file", "./data/The_Detective_Store/test_dialg.txt", "Dialog of characters")
+tf.flags.DEFINE_string("negative_data_file", "./data/The_Detective_Store/test_dialog.txt", "Dialog of characters")
 
 # Eval Parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (default: 64)")
-tf.flags.DEFINE_string("checkpoint_dir", "", "Checkpoint directory from training run")
-tf.flags.DEFINE_boolean("eval_train", False, "Evaluate on all training data")
+tf.flags.DEFINE_string("checkpoint_dir", "/home/kalab/github/cnn-text-classification-tf/runs/1518053281/checkpoints", "Checkpoint directory from training run")
+tf.flags.DEFINE_boolean("eval_train", True, "Evaluate on all training data")
 
 # Misc Parameters
 tf.flags.DEFINE_boolean("allow_soft_placement", True, "Allow device soft device placement")
